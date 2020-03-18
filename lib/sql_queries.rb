0bc71ef317f87b599fa2eb8c4ef46dbbe786c0c2
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-03-18 19:21:05 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,7 +26,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   "SELECT projects.title, projects.funding_goal - SUM(pledges.amount) AS over_goal FROM projects
     INNER JOIN pledges
     ON projects.id = pledges.project_id
-  
+
     GROUP BY projects.title"
 end
 
